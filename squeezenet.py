6f2071b362632997b7f734a09691539fc75ee0b6
thuBingo <53423415+thuBingo@users.noreply.github.com>
2020-02-11 16:32:26 +0800

    Create squeezenet.py

---
+++ squeezenet.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.functional as F
+import numpy as np
+import torch.optim as optim
+import math
+
+
+class fire(nn.Module):
+    def __init__(self, inplanes, squeeze_planes, expand_planes):
+        super(fire, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1, stride=1)
+        self.bn1 = nn.BatchNorm2d(squeeze_planes)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(squeeze_planes, expand_planes, kernel_size=1, stride=1)
+        self.bn2 = nn.BatchNorm2d(expand_planes)
+        self.conv3 = nn.Conv2d(squeeze_planes, expand_planes, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(expand_planes)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        # using MSR initilization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+                m.weight.data.normal_(0, math.sqrt(2./n))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        out1 = self.conv2(x)
+        out1 = self.bn2(out1)
+        out2 = self.conv3(x)
+        out2 = self.bn3(out2)
+        out = torch.cat([out1, out2], 1)
+        out = self.relu2(out)
+        return out
+
+
+#initial picture size = 256
+class SqueezeNet(nn.Module):
+    def __init__(self, class_num):
+        super(SqueezeNet, self).__init__()
+        self.class_num = class_num
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=1) # 32
+        self.bn1 = nn.BatchNorm2d(96)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2) # 16
+        self.fire2 = fire(96, 16, 64)
+        self.fire3 = fire(128, 16, 64)
+        self.fire4 = fire(128, 32, 128)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2) # 8
+        self.fire5 = fire(256, 32, 128)
+        self.fire6 = fire(256, 48, 192)
+        self.fire7 = fire(384, 48, 192)
+        self.fire8 = fire(384, 64, 256)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2) # 4
+        self.fire9 = fire(512, 64, 256)
+        self.softmax = nn.LogSoftmax(dim=1)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Conv2d(512, class_num, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+        x1 = self.fire2(x)
+        x2 = self.fire3(x1)
+        x = self.fire4(x1+x2)
+        x3 = self.maxpool2(x)
+        x4 = self.fire5(x3)
+        x5 = self.fire6(x3+x4)
+        x6 = self.fire7(x5)
+        x = self.fire8(x5+x6)
+        x7 = self.maxpool3(x)
+        x8 = self.fire9(x7)
+        x = self.classifier(x7+x8)
+        x = self.softmax(x)
+        return x.view(-1, self.class_num)
+
+def squeezenet(pretrained=False):
+    net = SqueezeNet()
+    # inp = Variable(torch.randn(64,3,32,32))
+    # out = net.forward(inp)
+    # print(out.size())
+    return net
